bea79a27e0ea594e20897904970af54799b1da0a
davorin <47928470+davorinpiljic@users.noreply.github.com>
2019-08-15 17:01:00 -0600

    Updated scheme-model, update() fix

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,5 +1,31 @@
 const db = require("../data/dbConfig.js");
 
-module.exports(find, findById, findSteps, add, update, remove);
+module.exports = { find, findById, findSteps, add, update, remove };
 
-function add() {}
+function find() {
+  return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes").where({ id });
+}
+
+function findSteps(id) {
+  return db("steps").where("scheme_id", id);
+}
+
+function add(scheme) {
+  return db("schemes").insert(scheme);
+}
+
+function update(changes, id) {
+  return db("schemes")
+    .where({ id })
+    .update(changes);
+}
+
+function remove(id) {
+  return db("schemes")
+    .where({ id })
+    .del();
+}
